6fe951e918e9d944bc68d322aabb56a48914bcbf
Ryan Eby <eby@ryaneby.com>
2016-08-17 16:32:11 -0400

    tasks for catalog stations and related files

---
+++ roles/catalog/files/desktop/autostart.sh
@@ -0,0 +1,5 @@
+# Turn off display blanking and launch chrome
+xset -dpms &
+xset s off &
+idesk &
+launch-chrome &
---
+++ roles/catalog/files/desktop/launch-chrome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# We use incognito mode so things aren't saved by default
+# We also set the user directory to /tmp so if anything manages to get saved a reboot will wipe
+
+google-chrome --incognito --no-first-run --start-maximized --disable-sync --disable-notifications -user-data-dir=/tmp/chrome
