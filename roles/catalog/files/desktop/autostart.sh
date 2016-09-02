bdf6ce5c9da8384d75dca78a1ec0365bd78537b5
Ryan Eby <eby@ryaneby.com>
2016-09-02 12:42:46 -0400

    install killall passwordless sudo and fix a hard-coded user

--- roles/catalog/files/desktop/autostart.sh
+++ roles/catalog/files/desktop/autostart.sh
@@ -1,5 +1,10 @@
 # Turn off display blanking and launch chrome
 xset -dpms &
 xset s off &
+xrandr -o left &
+xrandr --dpi 96
+xinput set-prop 12 "Evdev Axis Calibration" 0 4095 0 4095
+xinput set-prop 12 "Evdev Axis Inversion" 1, 0
+xinput set-prop 12 "Evdev Axes Swap" 1
 idesk &
 launch-chrome &
