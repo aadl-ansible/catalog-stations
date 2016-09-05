4561a445f58e3bfc0fa1e0d549f84157c78bd0e2
Ryan Eby <eby@ryaneby.com>
2016-09-05 12:50:29 -0400

    fix some mouse axis issues

--- roles/catalog/files/desktop/launch-chrome.sh
+++ roles/catalog/files/desktop/launch-chrome.sh
@@ -2,5 +2,9 @@
 
 # We use incognito mode so things aren't saved by default
 # We also set the user directory to /tmp so if anything manages to get saved a reboot will wipe
-
+export DISPLAY=:0.0
+xrandr --dpi 96
+xinput set-prop 12 "Evdev Axis Calibration" 0 4095 0 4095
+xinput set-prop 12 "Evdev Axis Inversion" 1, 0
+xinput set-prop 12 "Evdev Axes Swap" 1
 google-chrome --incognito --no-first-run --start-maximized --disable-sync --disable-notifications -user-data-dir=/tmp/chrome
