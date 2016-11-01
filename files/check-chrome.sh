62749ede992c3bf4221ae1a09f07379ab9a05849
Ryan Eby <eby@ryaneby.com>
2016-11-01 11:35:00 -0400

    remove idle check from check-chrome. fixes #15

--- files/check-chrome.sh
+++ files/check-chrome.sh
@@ -5,15 +5,4 @@ export DISPLAY=:0.0
 if ! [ -h /tmp/chrome/SingletonLock ]; 
 then
     /usr/bin/launch-chrome
-elif test `find "/tmp/chrome/SingletonLock" -mmin +7`
-	then
-# If chrome has been running for more than 7 minutes
-# and has been idle between 5-6.5 minutes then reset it
-		idle=$(xprintidle)
-		if [ $idle -ge 300000 ] && [ $idle -le 360000 ]
-		then
-			killall chrome
-			rm -r /tmp/chrome
-			/usr/bin/launch-chrome
-		fi
 fi
