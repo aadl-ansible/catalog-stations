2dc398550606173cb706a29cc85f0c075da39e4c
Ryan Eby <eby@ryaneby.com>
2018-03-06 17:23:57 -0500

    no calibrate for now

--- files/desktop/autostart.sh
+++ files/desktop/autostart.sh
@@ -1,8 +1,6 @@
 # Turn off display blanking and launch chrome
 xset -dpms &
 xset s off &
-xrandr -o left &
-/usr/bin/calibrate-display
 idesk &
 fbpanel &
 launch-chrome &
--- files/desktop/launch-chrome.sh
+++ files/desktop/launch-chrome.sh
@@ -2,5 +2,4 @@
 
 # We use incognito mode so things aren't saved by default
 # We also set the user directory to /tmp so if anything manages to get saved a reboot will wipe
-/usr/bin/calibrate-display
-google-chrome --incognito --no-first-run --start-maximized --disable-sync --disable-notifications -user-data-dir=/tmp/chrome http://www.aadl.org/catalog
+google-chrome --incognito --no-first-run --start-maximized --disable-sync --disable-notifications -user-data-dir=/tmp/chrome https://aadl.org/catalog
