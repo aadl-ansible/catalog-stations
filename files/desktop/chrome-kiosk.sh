973db28140afcfe35676b625887efe8967b2a9b1
Ryan Eby <ebyr@aadl.org>
2023-01-03 16:28:01 -0500

    reorg files

--- files/desktop/chrome-kiosk.sh
+++ files/desktop/chrome-kiosk.sh
@@ -2,4 +2,4 @@
 
 # We use incognito mode so things aren't saved by default
 # We also set the user directory to /tmp so if anything manages to get saved a reboot will wipe
-google-chrome --incognito --no-first-run --start-maximized --disable-sync --disable-notifications -user-data-dir=/tmp/chrome https://aadl.org/catalog
+chromium-browser --incognito --no-first-run --start-maximized --disable-sync --disable-notifications -user-data-dir=/tmp/chrome https://aadl.org/catalog
